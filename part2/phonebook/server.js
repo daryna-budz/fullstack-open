3400de918d718a2bb117d9e6726e07d6ecd4f354
daryna-budz <darynabudz@gmail.com>
2026-08-07 00:08:05 -0400

    part3(phonebook): ex.3.9

--- part2/phonebook/server.js
+++ part2/phonebook/server.js
@@ -1,9 +1,14 @@
 
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
+
+
+
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', (request) => {
   return JSON.stringify(request.body)
