6b0104800454ca4777834eecffabcd867a6b706e
daryna-budz <darynabudz@gmail.com>
2026-08-09 12:39:12 -0400

    phonebook: fixed vite.config for local development

--- part2/phonebook/vite.config.js
+++ part2/phonebook/vite.config.js
@@ -5,3 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+export default {
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      }
+    }
+  }
+}
