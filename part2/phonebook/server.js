0983390482d9668624369f7567c985df8206fa89
daryna-budz <darynabudz@gmail.com>
2026-08-16 22:55:35 -0400

    part 3(phonebook): ex.3.16

--- part2/phonebook/server.js
+++ part2/phonebook/server.js
@@ -26,37 +26,44 @@ app.use((request, response, next) => {
   }
 })
 
-app.get('/api/persons', (request, response) => {
+
+app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(persons => {
     response.json(persons)
   })
+  .catch((error) => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then((person)=>{
-      response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then((person)=>{
+      if(person){
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
+
+app.get('/info', (request, response, next) => {
   const time = new Date()
 
   Person.find({}).then((persons) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${time}</p>`)
   })
+  .catch((error) => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id).then((result) => {
     response.status(204).end()
   })
-  .catch((error) => {
-    console.log(error)
-    response.status(400).send({ error: 'malformatted id' })
-  })
+  .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if(!body.name || !body.number){
@@ -64,6 +71,7 @@ app.post('/api/persons', (request, response) => {
       error: 'Name or number is missing'
     })
   }
+
 
   Person.findOne({ name: body.name }).then(existingPerson => {
   if (existingPerson) {
@@ -80,8 +88,21 @@ app.post('/api/persons', (request, response) => {
   person.save().then((savedPerson) => {
     response.json(savedPerson)
   })
+  .catch((error) => next(error))
 })
+.catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError'){
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 
