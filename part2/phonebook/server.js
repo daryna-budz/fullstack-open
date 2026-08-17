844b4bbfecf1f161785f4a0e417260f871e1d50b
daryna-budz <darynabudz@gmail.com>
2026-08-16 22:22:07 -0400

    part 3(phonebook): ex. 3.15

--- part2/phonebook/server.js
+++ part2/phonebook/server.js
@@ -47,7 +47,7 @@ app.get('/info', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndRemove(request.params.id).then((result) => {
+  Person.findByIdAndDelete(request.params.id).then((result) => {
     response.status(204).end()
   })
   .catch((error) => {
